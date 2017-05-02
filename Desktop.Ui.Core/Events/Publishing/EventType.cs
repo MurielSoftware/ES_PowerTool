64e6bb3d034a29e036eaccefb30a8734a32123cb
Jaromír Krpec <krpec.jaromir@seznam.cz>
2017-05-02 22:38:32 +0200

    ESPT-003: publish

---
+++ Desktop.Ui.Core/Events/Publishing/EventType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desktop.Ui.Core.Events.Publishing
+{
+    public enum EventType
+    {
+        CREATE,
+        UPDATE,
+        DELETE,
+    }
+}
---
+++ Desktop.Ui.Core/Events/Publishing/Publisher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desktop.Ui.Core.Events.Publishing
+{
+    public class Publisher
+    {
+        private static Publisher instance;
+        private ICollection<IPublishListener> _listeners = new List<IPublishListener>();
+
+        public static Publisher GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Publisher();
+            }
+            return instance;
+        }
+
+        public void AddListener(IPublishListener listener)
+        {
+            _listeners.Add(listener);
+        }
+
+        public void RemoveListener(IPublishListener listener)
+        {
+            _listeners.Remove(listener);
+        }
+
+        public void Publish(PublishEvent publishEvent)
+        {
+            foreach (IPublishListener listener in _listeners)
+            {
+                listener.OnEvent(publishEvent);
+            }
+        }
+    }
+}
